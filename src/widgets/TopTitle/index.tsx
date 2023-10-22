2abe31fd9e3d30b92449e461bd7f52a025e9b377
devcowsar <kawsarcse836@gmail.com>
2023-10-22 06:10:32 -0700

    fix: top title

--- src/widgets/TopTitle/index.tsx
+++ src/widgets/TopTitle/index.tsx
@@ -7,27 +7,25 @@ import { TopTitleProps } from "./Types";
 const TopTitle: FC<TopTitleProps> = ({ title, subtitle }) => {
   return (
     <>
-      <Stack
-        justifyContent="center"
-        alignItems="center"
-        sx={(theme) => ({ color: theme.palette.grey[400], opacity: 0.2 })}
-      >
-        <Typography variant="h1">{title}</Typography>
+      <Stack textAlign="center">
+        <Box sx={(theme) => ({ color: theme.palette.grey[400], opacity: 0.2 })}>
+          <Typography variant="h1">{title}</Typography>
+        </Box>
+        <Box position="absolute" alignSelf="center" pt={7}>
+          <Typography fontWeight={700} lineHeight={1.5} variant="h4">
+            {subtitle}
+          </Typography>
+          <Divider
+            sx={(theme) => ({
+              borderBottomWidth: 4,
+              borderColor: theme.palette.success.light,
+            })}
+            light
+            variant="middle"
+            textAlign="center"
+          />
+        </Box>
       </Stack>
-      <Box position="absolute" left={830} top={130}>
-        <Typography fontWeight={700} variant="h4">
-          {subtitle}
-        </Typography>
-        <Divider
-          sx={(theme) => ({
-            borderBottomWidth: 4,
-            borderColor: theme.palette.success.light,
-          })}
-          light
-          variant="middle"
-          textAlign="center"
-        />
-      </Box>
     </>
   );
 };
